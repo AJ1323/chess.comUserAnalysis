1970835f79152f45ca37b26247c2b818e18f599d
AJ1323 <u1246172@umail.utah.edu>
2025-03-26 11:53:27 -0600

    Deleted commented out portion of the HandleClick function.

--- GamesGatherer/chessgamegatherer/src/components/EntryBox.js
+++ GamesGatherer/chessgamegatherer/src/components/EntryBox.js
@@ -44,7 +44,7 @@ const EntryBox = () => {
                         results.set( 'losses', losses + 1 ) ;
                         break;
                     case 'stalemate':
-                        results.set( 'ldraws', draws + 1 ) ;
+                        results.set( 'draws', draws + 1 ) ;
                         break;
                     case 'lose':
                         results.set( 'losses', losses + 1 ) ;
@@ -127,109 +127,11 @@ const EntryBox = () => {
             const statsJson = await response.json();
             let games = statsJson.games;
             
-            
-           // games.foreach( parseGames )
-
-           
-
-           
             const newStats = parseGames(games);
-           
-
-        //    for( let i = 0; i < games.length; i++)
-        //     { 
-                
-        //         if(games[i].white.username === name){
-
-        //             switch( games[i].white.result ){
         
-        //                 case 'checkmated':
-        //                     addLoss( losses + 1 );
-        //                     break;
-        //                 case 'agreed':
-        //                     addDraw( draws + 1 );
-        //                     break
-        //                 case 'repetition':
-        //                     addDraw( draws + 1 );
-        //                     break;
-        //                 case 'timeout':
-        //                     addLoss( losses + 1 );
-        //                     break;
-        //                 case 'resigned':
-        //                     addLoss( losses + 1 );
-        //                     break;
-        //                 case 'stalemate':
-        //                     addDraw( draws + 1 );
-        //                     break;
-        //                 case 'lose':
-        //                     addLoss( losses + 1 );
-        //                     break;
-        //                 case 'insufficient':
-        //                     addDraw( draws + 1 );
-        //                     break;
-        //                 case '50move':
-        //                     addDraw( draws + 1 );
-        //                     break;
-        //                 case 'abandoned':
-        //                     addLoss( losses + 1 );
-        //                     break;
-        //                 case 'timevsinsufficient':
-        //                     addDraw( draws + 1 );
-        //                     break;
-        //                 default:
-        //                     addWin( wins + 1 );
-        //             }
-        //             //addOpponents( opponents.push(game.black.username) );
-        //         }   
-        //         else{
-        
-        //             switch( games[i].black.result ){
-        
-        //                 case 'checkmated':
-        //                     addLoss( losses + 1 );
-        //                     break;
-        //                 case 'agreed':
-        //                     addDraw( draws + 1 );
-        //                     break
-        //                 case 'repetition':
-        //                     addDraw( draws + 1 );
-        //                     break;
-        //                 case 'timeout':
-        //                     addLoss( losses + 1 );
-        //                     break;
-        //                 case 'resigned':
-        //                     addLoss( losses + 1 );
-        //                     break;
-        //                 case 'stalemate':
-        //                     addDraw( draws + 1 );
-        //                     break;
-        //                 case 'lose':
-        //                     addLoss( losses + 1 );
-        //                     break;
-        //                 case 'insufficient':
-        //                     addDraw( draws + 1 );
-        //                     break;
-        //                 case '50move':
-        //                     addDraw( draws + 1 );
-        //                     break;
-        //                 case 'abandoned':
-        //                     addLoss( losses + 1 );
-        //                     break;
-        //                 case 'timevsinsufficient':
-        //                     addDraw( draws + 1 );
-        //                     break;
-        //                 default:
-        //                     addWin( wins + 1 );
-        //                     //addOpponents( opponents.push(game.white.username) );
-        //             }
-        
-                    
-        //         }
-
-        //     }
-                
-
             setStats( newStats );
+
+            
 
         } catch (err) {
             console.log(err.message)
@@ -241,13 +143,19 @@ const EntryBox = () => {
   return (
     
     <div>
-        <input className='name_input' required="required" placeholder='Enter a UserName' value={name} onChange={e => setName(e.target.value)} />
+        <h1>
+            Shitty Monthly Game Checker
+        </h1>
+        <input className='name_input' required="required" 
+        placeholder='Enter a UserName' value={name}
+         onChange={e => setName(e.target.value)} />
 
         <button type="submit" onClick={handleClick} >Submit</button>
 
         <label className='stats_display' />
         { stats }
         <label/>
+
         
     </div>
       
